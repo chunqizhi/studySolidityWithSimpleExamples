295de297edb52dd79def74c58dc70f4184101ba5
chunqizhi <1558763837@qq.com>
2021-01-06 14:07:24 +0800

    Add

---
+++ callingOtherContract.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.7.6;
+
+contract Callee {
+    uint public x;
+    uint public value;
+
+    function setX(uint _x) public returns (uint) {
+        x = _x;
+        return x;
+    }
+
+    function setXandSendEther(uint _x) public payable returns (uint, uint) {
+        x = _x;
+        value = msg.value;
+        return (x, value);
+    }
+}
+
+contract Caller {
+    event Log(uint,uint);
+    function setX(Callee _callee, uint _x) public {
+        uint x = _callee.setX(_x);
+        emit Log(x,0);
+    }
+
+    function setXFromAddress(address _addr, uint _x) public {
+        Callee callee = Callee(_addr);
+        callee.setX(_x);
+    }
+
+    function setXandSendEther(Callee _callee, uint _x) public payable {
+    (uint x, uint value) = _callee.setXandSendEther{value: msg.value}(_x);
+        emit Log(x,value);
+    }
+}
