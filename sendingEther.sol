c3ddc1315eb3f4b7b355eb76c7eaa2715dcdc476
chunqizhi <1558763837@qq.com>
2021-01-06 11:33:08 +0800

    Modify

--- sendingEther.sol
+++ sendingEther.sol
@@ -36,7 +36,7 @@ contract SendEther {
     function sendViaSend(address payable _to) public payable {
         // Send returns a boolean value indicating success or failure.
         // This function is not recommended for sending Ether.
-        bool sent = _to.send(mgs.value);
+        bool sent = _to.send(msg.value);
         require(sent, "Failed to send Ether");
     }
 
