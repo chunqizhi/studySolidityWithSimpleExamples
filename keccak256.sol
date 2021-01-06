aadc7b4811d681fc1473bc099fa63c34b27ffb69
chunqizhi <1558763837@qq.com>
2021-01-06 15:44:53 +0800

    Add

---
+++ keccak256.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.7.6;
+
+contract HashFunction {
+    function hash(string memory _text, uint _num, address _addr) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_text, _num, _addr));
+    }
+
+    // Example of hash collision
+    // Hash collision can occur when you pass more than on dynamic data type to abi.encodePacked. In such case, you should use abi.encode instead
+    function collision(string memory _text, string memory _anotherText) public pure returns (bytes32) {
+        // encodePacked(aaa, bbb) -> aaabbb
+        // encodePacked(aa, abbb) -> aaabbb
+        return keccak256(abi.encodePacked(_text, _anotherText));
+    }
+}
+
+contract GuessTheMagicWord {
+    bytes32 public answer = 0x60298f78cc0b47170ba79c10aa3851d7648bd96f2f8e46a19dbc777c36fb0c00;
+
+    // Magic word is "Solidity"
+    function guess(string memory _word) public view returns (bool) {
+        return keccak256(abi.encodePacked(_word)) == answer;
+    }
+}
